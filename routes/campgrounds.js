86332b3ea3223a80e12c6bfed85e6f8e6d623b69
EvilSmoothie <8687234+EvilSmoothie@users.noreply.github.com>
2023-02-16 00:49:16 +0000

    Added router routes

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -11,14 +11,17 @@ const {validateCampground} = require('../middleware');
 
 
 
+router.route('/')
+   .get(catchAsync(campgrounds.index))
+   .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
 
-router.get('/', catchAsync(campgrounds.index))
 router.get('/new', isLoggedIn, campgrounds.renderNewForm) 
-router.post('/', isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
-router.get('/:id', catchAsync(campgrounds.showCampground))
-router.get('/:id/edit',isLoggedIn, isAuthor ,catchAsync(campgrounds.renderEditForm))
-router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground))
-router.delete('/:id', isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground))
 
+router.route('/:id')
+   .get(catchAsync(campgrounds.showCampground))
+   .put(isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground))
+   .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground))
+
+router.get('/:id/edit',isLoggedIn, isAuthor ,catchAsync(campgrounds.renderEditForm))
 
 module.exports = router;
